7e9e9afe1a6734e579e0f2c5b8e53af96f063b47
Eleftheria Batsou <eleftheriabatsou@gmail.com>
2024-05-02 16:30:57 +0300

    Add files via upload

---
+++ client/src/main.rs
@@ -0,0 +1,59 @@
+use std::io::{self, ErrorKind, Read, Write};
+use std::net::TcpStream;
+use std::sync::mpsc::{self, TryRecvError};
+use std::thread;
+use std::time::Duration;
+
+const LOCAL: &str = "127.0.0.1:6000";
+const MSG_SIZE: usize = 32;
+
+fn main() {
+    let mut client = TcpStream::connect(LOCAL).expect("Stream failed to connect");
+    client.set_nonblocking(true).expect("failed to initiate non-blocking");
+
+    let (tx, rx) = mpsc::channel::<String>();
+
+    thread::spawn(move || loop {
+        let mut buff = vec![0; MSG_SIZE];
+        match client.read_exact(&mut buff) {
+            Ok(_) => {
+                let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                println!("message recv {:?}", msg);
+            },
+            Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+            Err(_) => {
+                println!("connection with server was severed");
+                break;
+            }
+        }
+
+        match rx.try_recv() {
+            Ok(msg) => {
+                let mut buff = msg.clone().into_bytes();
+                buff.resize(MSG_SIZE, 0);
+                client.write_all(&buff).expect("writing to socket failed");
+                println!("message sent {:?}", msg);
+            }, 
+            Err(TryRecvError::Empty) => (),
+            Err(TryRecvError::Disconnected) => break
+        }
+
+        thread::sleep(Duration::from_millis(100));
+    });
+
+    println!("Write a Message:");
+    loop {
+        let mut buff = String::new();
+        io::stdin().read_line(&mut buff).expect("reading from stdin failed");
+        let msg = buff.trim().to_string();
+        if msg == ":quit" || tx.send(msg).is_err() {break}
+    }
+    println!("bye bye!");
+
+}
+
+// To run this program you need to open 2 terminals. One for the client and one for the server. 
+// In the server run `cargo run`. 
+// Then do the same in your client. And this time you should see a message, `write a message`. 
+// Type something and then you should see that in the server. 
+// If you type ':quit' then the program will quit...
